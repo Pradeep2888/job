11ae85616b689761cf06c70e38b00702b1fcce1e
Pradeep2888 <pradeeptiwari2888@gmail.com>
2022-12-17 19:09:09 +0530

    pradeep

--- routes/job.route.js
+++ routes/job.route.js
@@ -10,7 +10,7 @@ const { JobModel } = require("../models/JobModel");
 
 jobRouter.get("/",async(req,res)=>{
     const {page,filter}=req.query
-    console.log(page)
+   
     if( filter==="" && Number(page)>1){
         const data=await JobModel.find().limit(10).skip(Number(page)*10)
         console.log(data)
