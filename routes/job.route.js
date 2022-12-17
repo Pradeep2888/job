56f806a8fca374dceb478c7d5e62c543d1c42628
Pradeep2888 <pradeeptiwari2888@gmail.com>
2022-12-17 11:57:42 +0530

    first commit

---
+++ routes/job.route.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const jobRouter=express.Router()
+const fs=require("fs")
+const dotenv=require("dotenv");
+
+const { time } = require("console");
+const { JobModel } = require("../models/JobModel");
+
+
+
+jobRouter.get("/",async(req,res)=>{
+    const data=await JobModel.find()
+    res.send({"data":data,"msg":"hi"})
+})
+
+jobRouter.post("/addjob",async(req,res)=>{
+    const {company,postedAt,city,location,role,level,contract,position,language}=req.body
+    
+    const new_job= new JobModel({
+       company,
+       postedAt,
+       city,
+       location,
+       role,
+       level,
+       contract,
+       position,
+       language
+    })
+    await new_job.save()
+
+    res.send({new_job})
+})
+
+
+module.exports={jobRouter}
